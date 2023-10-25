1d81427e1192013796a4112c6f7ccdee4b6b3a17
unknown <aliframadhani2002@gmail.com>
2023-10-25 03:02:44 -0600

    keren

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BuildingComponent } from './building/building.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BuildingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
